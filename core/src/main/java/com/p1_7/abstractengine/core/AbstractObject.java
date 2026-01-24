47d1f555606b97ef14a48e342d695168cb8115c4
timothyckl <timothy.ckl@outlook.com>
2026-01-24 13:15:48 +0800

    remove unused code

--- core/src/main/java/com/p1_7/abstractengine/core/AbstractObject.java
+++ core/src/main/java/com/p1_7/abstractengine/core/AbstractObject.java
@@ -6,14 +6,11 @@ public abstract class AbstractObject {
     protected boolean active;
     protected Array<AbstractProperty> properties;
 
-    public abstract void update(float deltaTime);
-
     public abstract boolean isActive();
 
     public abstract void addProperty(AbstractProperty property);
 
     public abstract void removeProperty(AbstractProperty property);
 
-    // public abstract AbstractElement getProperty(Object type); // note: me thinks this will need a revision.
     public abstract <T extends AbstractProperty> T getProperty(Class<T> type);
 }
