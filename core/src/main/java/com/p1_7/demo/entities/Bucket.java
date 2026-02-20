3a077dff9ca372eb5d1a8c6b7d8612e6d0e0a331
timothyckl <timothy.ckl@outlook.com>
2026-02-21 04:26:53 +0800

    remove unused imports

--- core/src/main/java/com/p1_7/demo/entities/Bucket.java
+++ core/src/main/java/com/p1_7/demo/entities/Bucket.java
@@ -2,8 +2,6 @@ package com.p1_7.demo.entities;
 
 import com.p1_7.abstractengine.collision.ICollidable;
 import com.p1_7.abstractengine.input.IInputQuery;
-import com.p1_7.demo.Settings;
-import com.p1_7.demo.core.Rectangle2D;
 import com.p1_7.demo.core.SpriteEntity;
 import com.p1_7.demo.input.DemoActions;
 import com.p1_7.demo.input.DropletCatchHandler;
